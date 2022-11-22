8b8a7d9cc63ac4261004904f42a8d974e835c110
A.L.I.C.E <serlifa@gmail.com>
2022-11-23 01:46:04 +0700

    add login

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,21 @@
 var express = require('express');
 var router = express.Router();
-const fs = require('fs');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'E-RA' });
-});
-
-router.get('/login', function(req, res, next) {
   res.render('login', { title: 'E-RA' });
 });
 
-router.get('/maps', function(req, res, next) {
-  res.render('maps', {
-    title: 'E-RA',
-  });
+router.post('/maps', function(req, res, next) {
+  if (
+      req.body.username == "era-unand" &
+      req.body.password == "EraUnand2022*"
+  ){
+    res.render('maps', {
+      title: 'E-RA',
+    });
+  }else{
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
